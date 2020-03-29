6f91945505d99bdeac49b6f3a6d17cf0e30d4130
huxin <18162650785@163.com>
2020-03-29 11:43:10 +0800

    Test

--- src/main/java/com/example/study/demo/StudyDemoApplication.java
+++ src/main/java/com/example/study/demo/StudyDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("123");
         SpringApplication.run(StudyDemoApplication.class, args);
     }
 
